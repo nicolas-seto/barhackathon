0c6398ea41c591fd448984d3d8b2778e385b00d4
nicolas-seto <nicolasseto@gmail.com>
2013-11-15 20:49:43 -0800

    Added some comments to handleMessage()

--- java/Contest2013/src/com/barracuda/contest2013/ContestBot.java
+++ java/Contest2013/src/com/barracuda/contest2013/ContestBot.java
@@ -5,6 +5,7 @@
 package com.barracuda.contest2013;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class ContestBot {
 	private static final int RECONNECT_TIMEOUT = 15; // seconds
@@ -60,58 +61,37 @@ public class ContestBot {
 			}
 
 			if (m.request.equals("request_card")) {
+			    /* We are first to play and can play card or issue challenge*/
 				if (m.state.can_challenge) {
-					//this where we put stuff
-
-					if(m.state.card > 0){
-						System.out.println("Their hand: " + m.state.card);
-						int[] currentHand = m.state.hand;
-						int minimumToWin = 0, highestToLose = 0;
-						int minIndex = 0, highIndex = 0;
-
-						for(int i = 0; i < currentHand.length; i++){
-							int difference = currentHand[i]-m.state.card;
-
-							//this finds the lowest possible card to beat their card
-							if(minimumToWin > difference && difference > 0){
-								minimumToWin = difference;
-								minIndex = i;
-							}
-							//dumps lowest card if we cant beat
-							else if(highestToLose > difference && highestToLose < 0){
-								highestToLose = difference;
-								highIndex = i;
-							}
-							else{	//tie
-
-							}
-						}//end forloop
-
-						int indextoUse = (int)(Math.random() * m.state.hand.length);;
-
-						if(minimumToWin != 0){ //if we can win
-							indextoUse = minIndex;
-							System.out.println("going to win");
-							
-						}
-						else{ //we will lose
-							indextoUse = highIndex;
-							System.out.println("going to lose");
-						}
-
-						return new PlayCardMessage(m.request_id,m.state.hand[indextoUse]);
-
-					}
-					else{
-						int i = (int)(Math.random() * m.state.hand.length);
-						System.out.println("Randomed:" + m.state.hand[i]);
-						return new PlayCardMessage(m.request_id, m.state.hand[i]);
-
-					}
-
+				   
 				}
-				else {
-					return new OfferChallengeMessage(m.request_id);
+				else { /* We are second to play and can only play card */
+				    int indextie = -1;
+				    int minWinIndex = -1;
+				    int minLoseIndex = 0;
+
+                    System.out.println("Their hand: " + m.state.card);
+                    int[] currentHand = m.state.hand;
+                    Arrays.sort(currentHand);
+
+                    for(int i = 0; i < currentHand.length; i++) {
+                        int difference = currentHand[i] - m.state.card;
+                        
+                        if (difference == 0) {
+                            /* Stores index */
+                            indextie = i;
+                        }
+                        if (difference > 0) {
+                            /* The first card we have that is higher, we play it */
+                            return new PlayCardMessage(m.request_id,m.state.hand[i]);
+                        } else if (i == currentHand.length - 1) {
+                            /* All our cards are lower or can tie, just play lowest card because the highest of our lowest can still win
+                             * further hands
+                             */
+                            return new PlayCardMessage(m.request_id,m.state.hand[0]);
+                        }
+                    }
+                    
 				}
 			}
 			else if (m.request.equals("challenge_offered")) {
